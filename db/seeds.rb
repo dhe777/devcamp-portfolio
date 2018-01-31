2257e2a20c2216e2576037d3edb6a2385c29f1c1
dhe777 <33729890+dhe777@users.noreply.github.com>
2018-01-30 19:03:12 -0700

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,7 +49,16 @@ end
     )
 
   puts "9 Portfolio items created"
-  
+
+  3.times do |technology|
+    Portfolio.last.technologies.create!(
+      name: "Technology #{technology}"
+    )
 end
+
+puts "3 technologies created"
+
+end
+
 
  
