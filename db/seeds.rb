5252d8c22e7d95b12ac112782c3f0bea0bdd9d97
dhe777 <dhe777@icloud.com>
2018-05-21 10:43:35 -0600

    Redo of secretfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+User.create!(
+  email: "test@test.com",
+  password: "Jael1324$",
+  password_confirmation: "Jael1324$",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
