8ad6ff80db41c3844cd53eb8221f8dc6180a2153
dhe777 <dhe777@icloud.com>
2018-05-21 10:03:24 -0600

    Added Admin and Regular User to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+User.create!(
+  email: "test@test.com",
+  password: "Jael1324$",
+  password_confirmation: "Jael1324$",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "Jael1324$",
+  password_confirmation: "Jael1324$",
+  name: "Regular User",
+)
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
